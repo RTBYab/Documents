2b2920efeadfdcd14c45294571e14a3204f2fe4d
Ali Azmoodeh Valadi <treeroot.ir@gmail.com>
2024-02-26 00:59:39 +0330

    Refactored code

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,8 +54,6 @@ const config = {
     themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
-            // Replace with your project's social card
-            image: 'img/docusaurus-social-card.jpg',
             navbar: {
                 title: 'ویدپروتکت',
                 logo: {
@@ -68,10 +66,6 @@ const config = {
                         sidebarId: 'docs',
                         position: 'left',
                         label: 'آموزش',
-                    },
-                    {
-                        type: 'localeDropdown',
-                        position: 'left'
                     }
                 ],
             },
