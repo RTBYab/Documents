593dc93c81b88815123675eaa6de362c7182c291
Ali Azmoodeh Valadi <treeroot.ir@gmail.com>
2024-04-08 18:10:09 +0330

    * Refactored code * Updated npm dependencies

--- scripts/builder.js
+++ scripts/builder.js
@@ -11,40 +11,43 @@ const inDevelopment = utils.getEnvType() === 'dev';
 makeSrc();
 
 function makeSrc() {
-    if (fs.existsSync(productionPath))
-        utils.deleteFolderRecursive(productionPath);
+  if (fs.existsSync(productionPath)) {
+    utils.deleteFolderRecursive(productionPath);
+  }
 
-    const ignoreFilePath = `${rootPath}.vidignore`;
+  const ignoreFilePath = `${rootPath}.vidignore`;
 
-    const ignoreList = fs.readFileSync(ignoreFilePath).toString().split(isLinux ? '\n' : '\r\n');
+  const ignoreList = fs.readFileSync(ignoreFilePath).
+    toString().
+    split(isLinux ? '\n' : '\r\n');
 
-    fs.mkdirSync(productionAppPath, {recursive: true});
+  fs.mkdirSync(productionAppPath, {recursive: true});
 
-    const files = fs.readdirSync(rootPath);
+  const files = fs.readdirSync(rootPath);
 
-    for (const file of files) {
-        const fullPath = path.join(rootPath, file);
-        const targetPath = path.join(productionAppPath, file);
+  for (const file of files) {
+    const fullPath = path.join(rootPath, file);
+    const targetPath = path.join(productionAppPath, file);
 
-        if (['liara.json'].includes(file) && inDevelopment) {
-            const newPath = targetPath.replace('/app', '');
-            fs.copyFileSync(fullPath, newPath);
-        }
-
-        if (ignoreList.includes(file))
-            continue;
-
-        if (fs.existsSync(fullPath)) {
-            const stats = fs.statSync(fullPath);
-
-            if (stats.isFile())
-                fs.copyFileSync(fullPath, targetPath);
-
-            if (stats.isDirectory())
-                utils.copyRecursiveSync(fullPath, targetPath);
-        }
-
+    if (['liara.json'].includes(file) && inDevelopment) {
+      const newPath = targetPath.replace('/app', '');
+      fs.copyFileSync(fullPath, newPath);
     }
 
-    require('./copyEnvs')('2', '../production/app');
+    if (ignoreList.includes(file)) {
+      continue;
+    }
+
+    if (fs.existsSync(fullPath)) {
+      const stats = fs.statSync(fullPath);
+
+      if (stats.isFile()) {
+        fs.copyFileSync(fullPath, targetPath);
+      }
+
+      if (stats.isDirectory()) {
+        utils.copyRecursiveSync(fullPath, targetPath);
+      }
+    }
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
-import { Redirect } from '@docusaurus/router';
+import React, {useEffect} from 'react';
+import {Redirect} from '@docusaurus/router';
 
 function Home() {
-    useEffect(() => {
-        window.location.href = '/docs/category/معرفی';
-    }, []);
+  useEffect(() => {
+    window.location.href = '/docs/category/معرفی';
+  }, []);
 
-    return <Redirect to="/docs/category/معرفی" />;
+  return <Redirect to="/docs/category/معرفی"/>;
 }
 
 export default Home;
